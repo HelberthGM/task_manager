046c6562077951cdbebddf1c85f6481e237ce24e
Helberth <helberth12@gmail.com>
2025-08-05 20:29:12 -0500

    removed db.sqlite3 and added to git ignore and create task model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import CASCADE
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_completed = models.DateTimeField(null=True)
+    urgent = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=CASCADE)
+
+    def __str__(self):  
+        return self.title + ' - by ' + self.user.username
+    
+    
