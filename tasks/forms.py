85f5712dddb34230da7ec22b5231d28c546ea9a7
Helberth <helberth12@gmail.com>
2025-08-16 18:47:05 -0500

    add styles to the task views and placeholder in forms

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,7 +1,12 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Task
 
-class TaskForm(ModelForm):
+class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['title', 'description', 'urgent']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Write the task title...'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Write a description of the task...'}),
+            'urgent': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+        }
